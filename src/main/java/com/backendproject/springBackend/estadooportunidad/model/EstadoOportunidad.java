2bd17db48b457754af258bad4a4946be6353a89e
Tadeo Gaviria <tadeogaviria@gmail.com>
2024-10-23 13:09:01 -0500

    feature/feature/estado_oportunidad/se crea y configura la tabla EstadoOportunidad se hacen pruebas CRUD todo OK

--- src/main/java/com/backendproject/springBackend/estadooportunidad/model/EstadoOportunidad.java
+++ src/main/java/com/backendproject/springBackend/estadooportunidad/model/EstadoOportunidad.java
@@ -1,4 +1,17 @@
 package com.backendproject.springBackend.estadooportunidad.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "estado_oportunidad")
+@Getter
+@Setter
 public class EstadoOportunidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String estado;
 }
