f239b323696b7a381ba9d107b9cc50b2b0bde1c7
Tadeo Gaviria <tadeogaviria@gmail.com>
2024-10-26 23:35:52 -0500

    feature/feature/categoria_oportunidad/Finalizacion Tarea categoria oportunidad

--- src/main/java/com/backendproject/springBackend/categoriaoportunidad/model/CategoriaOportunidad.java
+++ src/main/java/com/backendproject/springBackend/categoriaoportunidad/model/CategoriaOportunidad.java
@@ -1,4 +1,22 @@
 package com.backendproject.springBackend.categoriaoportunidad.model;
 
+import com.backendproject.springBackend.oportunidades.model.Oportunidades;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categoria_oportunidad")
+@Getter
+@Setter
 public class CategoriaOportunidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String categoria;
+
 }
