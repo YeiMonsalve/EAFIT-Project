78ea1aa4ef624ff8565843cb4f3e06cf35238c26
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-10-27 12:57:44 -0500

    CErrores corregidos de la tarea

--- src/main/java/com/backendproject/springBackend/tiposoportunidad/model/TiposOportunidad.java
+++ src/main/java/com/backendproject/springBackend/tiposoportunidad/model/TiposOportunidad.java
@@ -1,11 +1,7 @@
 package com.backendproject.springBackend.tiposoportunidad.model;
-import com.backendproject.springBackend.oportunidades.model.Oportunidades;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @Table(name = "tipos_oportunidad")
@@ -19,8 +15,4 @@ public class TiposOportunidad {
   private String nombre;
   private String descripcion;
 
-  @OneToMany(mappedBy = "tiposOporId", cascade = CascadeType.REMOVE)
-  @Setter(AccessLevel.NONE)
-  @Getter(AccessLevel.NONE)
-  private List<Oportunidades> oportunidades;
 }
