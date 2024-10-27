3184b59470cff1b6c016ae819b274efb259e1062
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-10-26 23:55:46 -0500

    CTarea 'instituciones' realizada con exito

--- src/main/java/com/backendproject/springBackend/instituciones/dto/InstitucionesJustIdDto.java
+++ src/main/java/com/backendproject/springBackend/instituciones/dto/InstitucionesJustIdDto.java
@@ -1,4 +1,11 @@
 package com.backendproject.springBackend.instituciones.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InstitucionesJustIdDto {
+
+    private int id;
 }
--- src/main/java/com/backendproject/springBackend/instituciones/model/Instituciones.java
+++ src/main/java/com/backendproject/springBackend/instituciones/model/Instituciones.java
@@ -1,4 +1,31 @@
 package com.backendproject.springBackend.instituciones.model;
 
+import com.backendproject.springBackend.institucionesoportunidades.model.InstitucionesOportunidades;
+import com.backendproject.springBackend.ubicacionregion.model.UbicacionRegion;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "instituciones")
+@Getter @Setter
 public class Instituciones {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String direccion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_region")
+    private UbicacionRegion idRegion;
+
+    @OneToMany(mappedBy = "institucionId", cascade = CascadeType.REMOVE)
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    private List<InstitucionesOportunidades> oportunidadesInstituciones;
 }
