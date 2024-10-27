228ca12ef67a73a675ae44ea8eccb11bb88180b6
Laurah2024 <laurahoyos2020@gmail.com>
2024-10-27 12:11:33 -0500

    feature/feature/tipos_oportunidad/tarea finalizada

--- src/main/java/com/backendproject/springBackend/tiposoportunidad/controller/TiposOportunidadController.java
+++ src/main/java/com/backendproject/springBackend/tiposoportunidad/controller/TiposOportunidadController.java
@@ -1,4 +1,45 @@
 package com.backendproject.springBackend.tiposoportunidad.controller;
 
+import com.backendproject.springBackend.tiposoportunidad.model.TiposOportunidad;
+import com.backendproject.springBackend.tiposoportunidad.service.TiposOportunidadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tipos-oportunidad")
 public class TiposOportunidadController {
+  @Autowired
+  private TiposOportunidadService tiposOportunidadService;
+
+  @PostMapping
+  public TiposOportunidad createTiposDeOportunidad(@RequestBody TiposOportunidad tiposOportunidad){
+    return tiposOportunidadService.createTipoDeOportunidad(tiposOportunidad);
+  }
+
+  @GetMapping
+  public List<TiposOportunidad> getAllTiposDeOportunidad(){
+    return tiposOportunidadService.tiposOportunidadFindAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<TiposOportunidad> getTiposDeOportunidadById(@PathVariable Long id){
+    Optional<TiposOportunidad> tiposOportunidad = tiposOportunidadService.tiposOportunidadFindById(id);
+    return tiposOportunidad.map(ResponseEntity::ok).orElseGet(() ->
+        ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<TiposOportunidad> updateTiposDeOportunidad(@PathVariable Long id, @RequestBody TiposOportunidad tiposOportunidad){
+    return  ResponseEntity.ok(tiposOportunidadService.updateTiposDeOportunidad(id, tiposOportunidad));
+  }
+
+  @DeleteMapping("/{id}")
+  public  ResponseEntity<Void> deleteTiposDeOportunidad(@PathVariable Long id){
+    tiposOportunidadService.deleteTiposDeOportunidad(id);
+    return ResponseEntity.noContent().build();
+  }
 }
--- src/main/java/com/backendproject/springBackend/tiposoportunidad/service/TiposOportunidadService.java
+++ src/main/java/com/backendproject/springBackend/tiposoportunidad/service/TiposOportunidadService.java
@@ -1,4 +1,39 @@
 package com.backendproject.springBackend.tiposoportunidad.service;
 
+import com.backendproject.springBackend.tiposoportunidad.model.TiposOportunidad;
+import com.backendproject.springBackend.tiposoportunidad.repository.TiposOportunidadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TiposOportunidadService {
+  @Autowired
+  private TiposOportunidadRepository tiposOportunidadRepository;
+
+  public TiposOportunidad createTipoDeOportunidad(TiposOportunidad tiposOportunidad){
+    return tiposOportunidadRepository.save(tiposOportunidad);
+  }
+
+  public Optional<TiposOportunidad> tiposOportunidadFindById(Long id){
+    return tiposOportunidadRepository.findById(id);
+  }
+
+  public List<TiposOportunidad> tiposOportunidadFindAll(){
+    return tiposOportunidadRepository.findAll();
+  }
+
+  public TiposOportunidad updateTiposDeOportunidad(Long id, TiposOportunidad details){
+    TiposOportunidad tiposOportunidad = tiposOportunidadRepository.findById(id).orElseThrow();
+    tiposOportunidad.setDescripcion(details.getDescripcion());
+    tiposOportunidad.setNombre(details.getNombre());
+    return tiposOportunidadRepository.save(tiposOportunidad);
+  }
+
+  public void deleteTiposDeOportunidad(Long id){
+    tiposOportunidadRepository.deleteById(id);
+  }
+
 }
